40066338fe92a4c1b2d95c7ef8783fa099fd65dd
nazarukiv <nazaruk7649@ukr.net>
2026-02-26 13:36:02 +0000

    project setup with dependencies

---
+++ src/main/java/com/nazarukiv/gitview/GitviewApplication.java
@@ -0,0 +1,13 @@
+package com.nazarukiv.gitview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nazarukiv/gitview/GitviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nazarukiv.gitview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
